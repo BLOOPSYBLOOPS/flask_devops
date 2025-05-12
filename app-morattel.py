672c47eca301e9587d69d75817c463db3f456cc5
BLOOPSYBLOOPS <morattel.bryan@gmail.com>
2025-05-12 11:31:25 +0200

    Test 2 du pipeline Jenkins

--- app-morattel.py
+++ app-morattel.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Bonjour, ceci est mon projet Flask DevOps - morattel test de jenkins WOW WOW WOW"
+    return "Bonjour, ceci est mon projet Flask DevOps - morattel test de jenkins WOW WOW WOW cest le deuxième test"
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
